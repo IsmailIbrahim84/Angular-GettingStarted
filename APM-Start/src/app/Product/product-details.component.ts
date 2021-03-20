8fce70bff44af572e2703f001202280b869d734c
IsmailIbrahim84 <beisso75@hotmail.com>
2021-03-20 21:07:08 +0200

    Adding routing

--- APM-Start/src/app/Product/product-details.component.ts
+++ APM-Start/src/app/Product/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+import {IProduct} from './product';
 
 @Component({
   selector: 'pm-product-details',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+pageTitle: string = 'Product Details';
+product: IProduct;
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      'productId': id,
+      'productName': 'Leaf Rake',
+      'productCode': 'GDN-0011',
+      'releaseDate': 'March 19 2019',
+      'description': 'Leaf rake with 48-inch wooden handle.',
+      'price': 19.95,
+      'starRating': 3.2,
+      'imageUrl': 'assets/images/leaf_rake.png'
+    }
   }
-
 }
